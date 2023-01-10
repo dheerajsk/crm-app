af902715bd99f714756544e3eeea03f0744c118f
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2023-01-10 21:27:33 +0530

    POST request

--- src/components/Customer/CustomerForm/CustomerForm.js
+++ src/components/Customer/CustomerForm/CustomerForm.js
@@ -1,11 +1,26 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function CustomerForm() {
-
   const [customer, setCustomer] = useState({});
+  const navigate = useNavigate();
 
-  function handleFormSubmit(){
+  function handleFormSubmit() {
     console.log(customer);
+    fetch("http://localhost:4000/api/customer", {
+      method: "POST",
+      body: JSON.stringify(customer),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((res) => {
+        console.log(res);
+        navigate("/");
+      });
   }
 
   return (
@@ -15,74 +30,77 @@ function CustomerForm() {
           Name
         </label>
         <input
-        onChange={
-          (e)=>{
-            customer.name = e.target.value; 
+          onChange={(e) => {
+            customer.name = e.target.value;
             setCustomer(customer);
           }}
-        type="text" className="form-control"></input>
+          type="text"
+          className="form-control"></input>
       </div>
       <div className="mb-3">
         <label htmlFor="exampleFormControlInput1" className="form-label">
           Website
         </label>
-        <input 
-        onChange={
-          (e)=>{
-            customer.website = e.target.value; 
+        <input
+          onChange={(e) => {
+            customer.website = e.target.value;
             setCustomer(customer);
           }}
-          type="text" className="form-control"></input>
+          type="text"
+          className="form-control"></input>
       </div>
       <div className="mb-3">
         <label htmlFor="exampleFormControlInput1" className="form-label">
           Turnover
         </label>
-        <input 
-        onChange={
-          (e)=>{
-            customer.turnover = e.target.value; 
+        <input
+          onChange={(e) => {
+            customer.turnover = e.target.value;
             setCustomer(customer);
           }}
-          type="number" className="form-control"></input>
+          type="number"
+          className="form-control"></input>
       </div>
       <div className="mb-3">
         <label htmlFor="exampleFormControlInput1" className="form-label">
           No Of Employees
         </label>
-        <input 
-        onChange={
-          (e)=>{
-            customer.employees = e.target.value; 
+        <input
+          onChange={(e) => {
+            customer.employees = e.target.value;
             setCustomer(customer);
           }}
-          type="number" className="form-control"></input>
+          type="number"
+          className="form-control"></input>
       </div>
       <div className="mb-3">
         <label htmlFor="exampleFormControlInput1" className="form-label">
           CEO
         </label>
-        <input 
-        onChange={
-          (e)=>{
-            customer.ceo = e.target.value; 
+        <input
+          onChange={(e) => {
+            customer.ceo = e.target.value;
             setCustomer(customer);
           }}
-          type="text" className="form-control"></input>
+          type="text"
+          className="form-control"></input>
       </div>
       <div className="mb-3">
         <label htmlFor="exampleFormControlInput1" className="form-label">
           Established In
         </label>
-        <input 
-        onChange={
-          (e)=>{
-            customer.year = e.target.value; 
+        <input
+          onChange={(e) => {
+            customer.year = e.target.value;
             setCustomer(customer);
           }}
-          type="number" className="form-control"></input>
+          type="number"
+          className="form-control"></input>
       </div>
-      <button onClick={handleFormSubmit} className="btn btn-primary float-end" type="button">
+      <button
+        onClick={handleFormSubmit}
+        className="btn btn-primary float-end"
+        type="button">
         Create New Customer
       </button>
     </div>
